d39d586529454c03cb49912bb0e8ac3677806a39
Joshua Sung <yuniksung@gmail.com>
2021-03-13 23:28:59 -0600

    homepage layout changed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,21 +6,27 @@ import '../../assets/scss/home.scss';
 function homePage() {
     return (
         <div className="homepage-container">
+            <div className="homepage-image">
 
-            <div className="homepage-textbox">
-                <h1 className="yunik-is">
-                    yunik.is<span>(Fullstack)</span>
-                </h1>
-                <p>
-                    A Talented,
-                    Skillful,
-                    Motivated and
-                    Web Developer
-                </p>
             </div>
-            <div className="home-description">
-                <p>Welcome to my portfolio, this website was built with <span>React.js</span>. Feel free to look around and if you have any questions, do not hesitate to contact me. You can reach out to me by clicking <span>Contact</span> menu.</p>
+
+            <div className="homepage-text">
+                <div className="homepage-textbox">
+                    <h1 className="yunik-is">
+                        yunik.is<span>(Fullstack)</span>
+                    </h1>
+                    <p>
+                        A Talented,
+                        Skillful,
+                        Motivated and
+                        Web Developer
+                    </p>
+                </div>
+                <div className="home-description">
+                    <p>Welcome to my portfolio, this website was built with <span>React.js</span>. Feel free to look around and if you have any questions, do not hesitate to contact me. You can reach out to me by clicking <span>Contact</span> menu.</p>
+                </div>
             </div>
+            
         </div>
     )
 }
